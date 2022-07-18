e4991eec0a318c67a49646bc550c81c9b8fc2aa1
quarkusio <no-reply@quarkus.io>
2022-07-18 10:27:24 +0000

    Initial commit

---
+++ src/test/java/com/bpi/microservices/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.bpi.microservices;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
